9e9b51e9cafae9e012f1cfce044a672bbd5bcd98
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2018-06-14 10:29:00 +0200

    Add fetch

--- js/App.js
+++ js/App.js
@@ -1,13 +1,13 @@
 // OGÓLNA FUNKCJA
-function randomString() {
-	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
+/*function randomString() {
+	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'();
 	var str = '', i;
 	for (i = 0; i < 10; i++) {
 	  str += chars[Math.floor(Math.random() * chars.length)];
 	}
 	return str;
 }
-
+*/
 function generateTemplate(name, data, basicElement) {
   	var template = document.getElementById(name).innerHTML;
   	var element = document.createElement(basicElement || 'div');
@@ -18,20 +18,32 @@ function generateTemplate(name, data, basicElement) {
   	return element;
 }
 
-// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
-var todoColumn = new Column('Do zrobienia');
-var doingColumn = new Column('W trakcie');
-var doneColumn = new Column('Skończone');
+// ŁACZENIE Z SERWEREM
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '3138',
+  'X-Auth-Token': 'e066a68301f5fcce95511c2b0ab3cb99'
+};
 
-// DODAWANIE KOLUMN DO TABLICY
-board.addColumn(todoColumn);
-board.addColumn(doingColumn);
-board.addColumn(doneColumn);
+fetch(baseUrl + '/board', {headers: myHeaders})
+	.then(function(resp) {
+		return resp.json();
+	})
+	.then(function(resp) {
+		setupColumns(resp.columns);
+	});
 
-// TWORZENIE NOWYCH EGZEMPLARZY KART
-var card1 = new Card('Nowe zadanie');
-var card2 = new Card('stworzyc tablice kanban');
+function setupColumns(columns) {
+  columns.forEach(function (column) {
+		var col = new Column(column.id, column.name);
+    board.addColumn(col);
+    setupCards(col, column.cards);
+  });
+}
 
-// DODAWANIE KART DO KOLUMN
-todoColumn.addCard(card1);
-doingColumn.addCard(card2);
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+    var cardObj = new Card(card.id, card.name);
+  	col.addCard(cardObj);
+	});
+}
--- js/Board.js
+++ js/Board.js
@@ -17,7 +17,21 @@ function initSortable(id) {
 }
 
 document.querySelector('#board .create-column').addEventListener('click', function() {
-    var name = prompt('Enter a column name');
-    var column = new Column(name);
-    board.addColumn(column);
+  var name = prompt('Enter a column name');
+  var data = new FormData();
+
+  data.append('name', name);
+
+  fetch(baseUrl + '/column', {
+      method: 'POST',
+      headers: myHeaders,
+      body: data,
+    })
+    .then(function(resp) {
+      return resp.json();
+    })
+    .then(function(resp) {
+      var column = new Column(resp.id, name);
+      board.addColumn(column);
+    });
 });
